d1bde3569a386e02f79f2395234634698f571004
Vladimir <djeksaly@gmail.com>
2024-07-17 15:33:09 +0300

    Some fix

--- src/main/java/com/digitalchief/tz/repository/CarShopRepo.java
+++
@@ -1,7 +0,0 @@
-package com.digitalchief.tz.repository;
-
-import com.digitalchief.tz.model.CarShop;
-import org.springframework.data.repository.CrudRepository;
-
-public interface CarShopRepo extends CrudRepository<CarShop, Long> {
-}
