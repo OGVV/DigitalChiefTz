b51ef8ea54eb3d61a7478fb2111e750a28f713d4
Vladimir <djeksaly@gmail.com>
2024-07-17 15:32:04 +0300

    Add controllers

---
+++ src/main/java/com/digitalchief/tz/controller/CarController.java
@@ -0,0 +1,82 @@
+package com.digitalchief.tz.controller;
+
+import com.digitalchief.tz.model.Car;
+import com.digitalchief.tz.model.CarShop;
+import com.digitalchief.tz.repository.CarRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class CarController {
+    @Autowired
+    ObjectMapper objectMapper;
+    @Autowired
+    CarRepository carRepository;
+    Car car;
+
+    @GetMapping(value = "/allCars", produces = "application/json")
+    public String allCarsView() {
+        List<Car> cars = carRepository.findAll();
+        try {
+            return objectMapper.writeValueAsString(cars);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @GetMapping(value = "/getCar/{id}", produces = "application/json")
+    public String shopView(@PathVariable Long id) {
+        Car car = carRepository.findById(id).orElseThrow();
+        try {
+            return objectMapper.writeValueAsString(car);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @PostMapping(value = "/addCar",produces = "application/json")
+    public HttpStatus addShop(@RequestBody String body) {
+        try {
+            Car car = objectMapper.readValue(body, Car.class);
+            carRepository.save(car);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+        return HttpStatus.CREATED;
+    }
+
+    @GetMapping(value = "/getCarsWithoutShop",produces = "application/json")
+    public String choseCars() {
+        List<Car> carsWithoutShop = carRepository.findByCarShopNull();
+        try {
+            return objectMapper.writeValueAsString(carsWithoutShop);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @PutMapping(value = "/updateCarInfo/{id}", produces = "application/json")
+    public HttpStatus updateShop(@RequestBody String body, @PathVariable Long id) {
+        try {
+            car = objectMapper.readValue(body, Car.class);
+            car.setId(id);
+            carRepository.save(car);
+            return HttpStatus.OK;
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @DeleteMapping(value = "/deleteCar/{id}", produces = "application/json")
+    public HttpStatus deleteShop(@PathVariable Long id) {
+        car = carRepository.findById(id).orElseThrow();
+        carRepository.delete(car);
+        return HttpStatus.OK;
+    }
+}
---
+++ src/main/java/com/digitalchief/tz/controller/CarShopController.java
@@ -0,0 +1,108 @@
+package com.digitalchief.tz.controller;
+
+import com.digitalchief.tz.model.Car;
+import com.digitalchief.tz.model.CarShop;
+import com.digitalchief.tz.repository.CarRepository;
+import com.digitalchief.tz.repository.CarShopRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.websocket.server.PathParam;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+
+@RestController
+public class CarShopController {
+
+    @Autowired
+    CarShopRepository carShopRepository;
+    @Autowired
+    ObjectMapper objectMapper;
+    @Autowired
+    CarRepository carRepository;
+    private CarShop carShop;
+
+    @GetMapping(value = "/allShops", produces = "application/json")
+    public String allShopsView() {
+        List<CarShop> carShops;
+        carShops = carShopRepository.findAll();
+        try {
+            return objectMapper.writeValueAsString(carShops);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @GetMapping(value = "/getShop/{id}", produces = "application/json")
+    public String shopView(@PathVariable Long id) {
+        carShop = carShopRepository.findById(id).orElseThrow();
+        try {
+            return objectMapper.writeValueAsString(carShop);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @PostMapping(value = "/createShop", produces = "application/json")
+    public HttpStatus addShop(@RequestBody String body) {
+        try {
+            carShop = objectMapper.readValue(body, CarShop.class);
+            carShopRepository.save(carShop);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+        return HttpStatus.CREATED;
+    }
+
+    @PostMapping(value = "/addCarsIntoShop/{id}", produces = "application/json")
+    public HttpStatus chooseCars(@PathVariable Long id, @RequestBody String body) {
+        carShop = carShopRepository.findById(id).orElseThrow();
+        try {
+            Set<Car> cars = objectMapper.readValue(body, new TypeReference<>() {
+            });
+            for (Car car :
+                    cars) {
+                carShop.addCar(car);
+            }
+            carShopRepository.save(carShop);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+        return HttpStatus.OK;
+    }
+
+    @PutMapping(value = "/updateShopInfo/{id}", produces = "application/json")
+    public HttpStatus updateShop(@RequestParam(value = "name",required = false) Optional<String> name,@RequestParam(value = "address",required = false) Optional<String> address, @PathVariable Long id) {
+        carShop = carShopRepository.findById(id).orElseThrow();
+        carShop.setCarShopName(name.orElse(carShop.getCarShopName()));
+        carShop.setCarShopAddress(address.orElse(carShop.getCarShopAddress()));
+        carShop.setId(id);
+        carShopRepository.save(carShop);
+        return HttpStatus.OK;
+    }
+
+    @DeleteMapping(value = "/deleteCarIntoShop/{id}", produces = "application/json")
+    public HttpStatus deleteCarsIntoShop(@PathVariable Long id, @RequestParam(value = "delete_car_id") Long carId) {
+        carShop = carShopRepository.findById(id).orElseThrow();
+        carShop.removeCar(carRepository.findById(carId).orElseThrow());
+        carShopRepository.save(carShop);
+        return HttpStatus.OK;
+    }
+
+    @DeleteMapping(value = "/deleteShop/{id}", produces = "application/json")
+    public HttpStatus deleteShop(@PathVariable Long id) {
+        carShop = carShopRepository.findById(id).orElseThrow();
+        for (Car car:
+             carShop.getCars()) {
+            car.setCarShop(null);
+        }
+        carShop.setCars(null);
+        carShopRepository.delete(carShop);
+        return HttpStatus.OK;
+    }
+}
